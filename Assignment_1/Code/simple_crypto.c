06f1d79648b1c9499d217edcd9c7ad23ee4c8095
sneakyPent <zaharioudakis@yahoo.com>
2020-10-13 18:32:13 +0300

    encryption and decryption functions

--- Assignment_1/Code/simple_crypto.c
+++ Assignment_1/Code/simple_crypto.c
@@ -2,48 +2,23 @@
 
 
 int main() {
-    int i;
     unsigned char* key;
-    key = randomKey( 8);
-    for(i = 0; i < 8; ++i)
-        printf("%02X", key[i]);
+    char plaintext[1024];
+    printf("[OTP] input:");
+    fgets(plaintext,1024,stdin);
+    int textSize = sizeof(plaintext);
+    key = randomKey(sizeof(textSize));
+
+    unsigned char* encr = OTP_encrypt(plaintext, key, textSize);
+    printf("[OTP] encrypted:");
+    for(int i = 0; i < textSize; ++i)
+        printf("%c", encr[i]);
+
+    unsigned char* decr = OTP_decrypt(encr, key, textSize);
+    printf("[OTP] decrypted:");
+    for(int i = 0; i < textSize; ++i)
+        printf("%c", decr[i]);
     printf("\n");
     return 0;
 }
 
-
-// #include "simple_crypto.h"
-//     #include <unistd.h>
-//     #include <fcntl.h>
-//     #include "string.h"
-
-//     int main(){
-//             int i;
-//             char str[8];
-//             ///
-//             printf("Type message to be encrypted: ");
-//             scanf("%s",str[8]);
-//             ///
-//             unsigned char buffer[8];
-//             int fd = open("/dev/urandom", O_RDONLY);
-//             read(fd, buffer, 8);
-//             //buffer now contains the random data
-//             close(fd);
-//             for(i = 0; i < 8; ++i)
-//                     printf("%02X", buffer[i]);
-//             printf("\n");
-
-//             char output[8];
-
-//             for (i=0; i<strlen(str); i++)
-//             {
-//                 char temp = str[i] ^ buffer[i];
-//                 output[i] = temp;
-//             }
-
-//             output[i] = '\0';
-//             printf("02Xs", output);
-
-
-//             return 0;
-//     }`11
--- Assignment_1/Code/simple_crypto.h
+++ Assignment_1/Code/simple_crypto.h
@@ -1,12 +1,28 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
 
-unsigned char*  randomKey(int size){
+unsigned char* randomKey(int size){
     unsigned char *buffer = (unsigned char *)malloc(sizeof(unsigned char )*size);
     int fd = open("/dev/urandom", O_RDONLY);
     read(fd, buffer, size);
     close(fd);
+    return buffer;
+}
+
+unsigned char* OTP_decrypt(unsigned char * str,unsigned char * key, int size){
+    unsigned char *buffer = (unsigned char *)malloc(sizeof(unsigned char )*size);
+    for (int i=0; i<strlen(str); i++)
+        buffer[i] = str[i] ^ key[i];
+    return buffer;
+}
+
+
+char* OTP_encrypt(char * str,char * key, int size){
+    char *buffer = (unsigned char *)malloc(sizeof(unsigned char )*size);
+    for (int i=0; i<strlen(str); i++)
+        buffer[i] = str[i] ^ key[i];
     return buffer;
 }
