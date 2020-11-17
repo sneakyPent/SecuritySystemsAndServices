7f9fcfcaf4ab39daeaa26e5e12cc77b765af95d1
sneakyPent <zaharioudakis@yahoo.com>
2020-11-17 23:53:46 +0200

    create function to get current time and date

--- Assignment_4/src_corpus/logger.h
+++ Assignment_4/src_corpus/logger.h
@@ -26,17 +26,40 @@ enum AccessType {creation = 0, opening = 1, writing = 2};
  */
 struct logEntry
 {
-	int UID; /** The unique user ID assigned by the system to a user */
-	char filename[4096]; /** The path and name of the accessed file. */
-	char date[BUF_LEN]; /** The date that the action occurred. */
-	char timestamp[BUF_LEN]; /** The time that the action occurred. */
-	enum AccessType access; /** For file creation 0. For file open 1. For file write 2 */
-	int isActionDenied; /** Reports if the action was denied to the user with no access privileges */
-	unsigned char fileFingerprint;  /** ​ The digital fingerprint of the file the time the event occurred. */
 
-};
+/**
+* @brief Get the Current Date And Time
+ * 
+ * @return char** 2D array [2][BUF_LEN] char[0]=time char[1]=date
+ */
+char **getCurrentDateAndTime()
+{
+	time_t current = time(NULL); /* Gets GMT time as a time_t. */
+	if (current == -1)
+	{
 
+		printf("The time() function failed");
+		exit(EXIT_FAILURE);
+	}
+	struct tm *pLocal = localtime(&current); /* Converts to local time in broken down format. */
+	if (pLocal == NULL)
+	{
 
+		printf("The localtime() function failed");
+		exit(EXIT_FAILURE);
+	}
+	char **dateAndTime;
 
+	dateAndTime = malloc(sizeof(char *) * 2);
+
+	for (int i = 0; i < 2; i++)
+	{
+		dateAndTime[i] = malloc(BUF_LEN);
+	}
+
+	strftime(dateAndTime[0], BUF_LEN, "%T", pLocal);
+	strftime(dateAndTime[1], BUF_LEN, "%d/%m/%Y", pLocal);
+	return dateAndTime;
+}
 
 #endif /* _LOGGER_H */
