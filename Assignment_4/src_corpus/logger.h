630d475bf14d9607c07faa53249b6fd8e5babe83
sneakyPent <zaharioudakis@yahoo.com>
2020-11-17 23:50:39 +0200

    add logEntry struct in logger.h

--- Assignment_4/src_corpus/logger.h
+++ Assignment_4/src_corpus/logger.h
@@ -19,6 +19,22 @@
  */
 enum AccessType {creation = 0, opening = 1, writing = 2};
 
+/**
+ * @brief Is a struct for all the information a log entry should contain
+ *   
+ * 
+ */
+struct logEntry
+{
+	int UID; /** The unique user ID assigned by the system to a user */
+	char filename[4096]; /** The path and name of the accessed file. */
+	char date[BUF_LEN]; /** The date that the action occurred. */
+	char timestamp[BUF_LEN]; /** The time that the action occurred. */
+	enum AccessType access; /** For file creation 0. For file open 1. For file write 2 */
+	int isActionDenied; /** Reports if the action was denied to the user with no access privileges */
+	unsigned char fileFingerprint;  /** ​ The digital fingerprint of the file the time the event occurred. */
+
+};
 
 
 
