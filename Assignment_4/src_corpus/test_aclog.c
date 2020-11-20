16eb3862c998c1fab240f8e1b40026e127f1a0a9
sneakyPent <zaharioudakis@yahoo.com>
2020-11-20 19:16:12 +0200

    added appending function for testing in test_aclog.c

--- Assignment_4/src_corpus/test_aclog.c
+++ Assignment_4/src_corpus/test_aclog.c
@@ -108,12 +108,58 @@ void writeFiles(){
 	}
 
 }
+void appendTest(){
+	int i;
+	size_t bytes;
+	FILE *file;
+	int files = 8;
+	char filenames[8][BUF_LEN] = {
+		"../testFiles/FILE_1.txt",
+		"../testFiles/FILE_2.txt",
+		"../testFiles/FILE_3.txt",
+		"../testFiles/FILE_4.txt",
+		"../testFiles/FILE_5.txt",
+		"../testFiles/FILE_6.txt",
+		"../testFiles/FILE_7.txt",
+		"/lnx/sneaky/testFiles/FILE_8.txt",
+	};
+	char nonPermFiles[8][BUF_LEN] = {
+		"../testFiles/N_FILE_1.txt",
+		"../testFiles/N_FILE_2.txt",
+		"../testFiles/N_FILE_3.txt",
+		"../testFiles/N_FILE_4.txt",
+		"../testFiles/N_FILE_5.txt",
+		"../testFiles/N_FILE_6.txt",
+		"../testFiles/N_FILE_7.txt",
+		"../testFiles/N_FILE_8.txt",
+	};
 
-	/* add your code here */
-	/* ... */
-	/* ... */
-	/* ... */
-	/* ... */
+	for (i = 0; i < files; i++) {
+		file = fopen(filenames[i], "a+");
+		print("Write", info);
+		if (file == NULL) 
+			printf("fopen error\n");
+		else {
+			char msg[BUF_LEN];
+			sprintf(msg," Append my uid %d", getuid());
+			bytes = fwrite(msg, strlen(msg), 1, file);
+			fclose(file);
+		}
+
+	}
+	for (i = 0; i < files; i++) {
+		file = fopen(nonPermFiles[i], "a+");
+		print("Write", info);
+		if (file == NULL) 
+			printf("fopen error\n");
+		else {
+			char msg[BUF_LEN];
+			sprintf(msg," Append my uid %d", getuid());
+			bytes = fwrite(msg, strlen(msg), 1, file);
+			fclose(file);
+		}
+
+	}
 
 
 }
