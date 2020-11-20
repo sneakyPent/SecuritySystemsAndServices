83d4965ab479824f9d8d3fb41d67e0df1fb29b7c
sneakyPent <zaharioudakis@yahoo.com>
2020-11-20 19:13:15 +0200

    added reading function for testing

--- Assignment_4/src_corpus/test_aclog.c
+++ Assignment_4/src_corpus/test_aclog.c
@@ -1,20 +1,53 @@
 #include <stdio.h>
 #include <string.h>
 
-int main() 
-{
+void readFiles(){
 	int i;
 	size_t bytes;
 	FILE *file;
-	char filenames[10][7] = {"file_0", "file_1", 
-			"file_2", "file_3", "file_4",
-			"file_5", "file_6", "file_7", 		
-			"file_8", "file_9"};
+	int files = 8;
+	char filenames[8][BUF_LEN] = {
+		"../testFiles/FILE_1.txt",
+		"../testFiles/FILE_2.txt",
+		"../testFiles/FILE_3.txt",
+		"../testFiles/FILE_4.txt",
+		"../testFiles/FILE_5.txt",
+		"../testFiles/FILE_6.txt",
+		"../testFiles/FILE_7.txt",
+		"../testFiles/FILE_8.txt",
+	};
+	char nonPermFiles[8][BUF_LEN] = {
+		"../testFiles/N_FILE_1.txt",
+		"../testFiles/N_FILE_2.txt",
+		"../testFiles/N_FILE_3.txt",
+		"../testFiles/N_FILE_4.txt",
+		"../testFiles/N_FILE_5.txt",
+		"../testFiles/N_FILE_6.txt",
+		"../testFiles/N_FILE_7.txt",
+		"../testFiles/N_FILE_8.txt",
+	};
 
+	for (i = 0; i < files; i++) {
+		file = fopen(filenames[i], "r");
+		print("Read", info);
+		if (file == NULL) 
+			printf("fopen error\n");
+		else {
+			fclose(file);
+		}
 
-	/* example source code */
+	}
+	for (i = 0; i < files; i++) {
+		file = fopen(nonPermFiles[i], "r");
+		print("Read", info);
+		if (file == NULL) 
+			printf("fopen error\n");
+		else {
+			fclose(file);
+		}
 
-	for (i = 0; i < 10; i++) {
+	}
+}
 
 		file = fopen(filenames[i], "w+");
 		if (file == NULL) 
