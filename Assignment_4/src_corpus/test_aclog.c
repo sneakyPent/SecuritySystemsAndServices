cd3cdccd71411886b81eb53bcd3d0f22bdebc44e
sneakyPent <zaharioudakis@yahoo.com>
2020-11-20 19:14:23 +0200

    added writing function for testing in test_aclog.c

--- Assignment_4/src_corpus/test_aclog.c
+++ Assignment_4/src_corpus/test_aclog.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <string.h>
+#include "utils.h"
+
+
+
 
 void readFiles(){
 	int i;
@@ -49,16 +53,61 @@ void readFiles(){
 	}
 }
 
+void writeFiles(){
+	
+	int i;
+	size_t bytes;
+	FILE *file;
+	int files = 8;
+	char filenames[8][BUF_LEN] = {
+		"../testFiles/FILE_1.txt",
+		"../testFiles/FILE_2.txt",
+		"../testFiles/FILE_3.txt",
+		"../testFiles/FILE_4.txt",
+		"../testFiles/FILE_5.txt",
+		"../testFiles/FILE_6.txt",
+		"../testFiles/FILE_7.txt",
+		"/lnx/sneaky/testFiles/FILE_8.txt",
+	};
+	char nonPermFiles[8][BUF_LEN] = {
+		"../testFiles/N_FILE_1.txt",
+		"../testFiles/N_FILE_2.txt",
+		"../testFiles/N_FILE_3.txt",
+		"../testFiles/N_FILE_4.txt",
+		"../testFiles/N_FILE_5.txt",
+		"../testFiles/N_FILE_6.txt",
+		"../testFiles/N_FILE_7.txt",
+		"../testFiles/N_FILE_8.txt",
+	};
+
+	for (i = 0; i < files; i++) {
 		file = fopen(filenames[i], "w+");
+		print("Write", info);
 		if (file == NULL) 
 			printf("fopen error\n");
 		else {
-			bytes = fwrite(filenames[i], strlen(filenames[i]), 1, file);
+			char msg[BUF_LEN];
+			sprintf(msg,"%d", getuid());
+			bytes = fwrite(msg, strlen(msg), 1, file);
+			fclose(file);
+		}
+
+	}
+	for (i = 0; i < files; i++) {
+		file = fopen(nonPermFiles[i], "w+");
+		print("Write", info);
+		if (file == NULL) 
+			printf("fopen error\n");
+		else {
+			char msg[BUF_LEN];
+			sprintf(msg,"%d", getuid());
+			bytes = fwrite(msg, strlen(msg), 1, file);
 			fclose(file);
 		}
 
 	}
 
+}
 
 	/* add your code here */
 	/* ... */
