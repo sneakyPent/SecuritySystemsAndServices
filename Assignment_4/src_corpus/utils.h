fc06a70ea6e2dcc08aaa415e84d44c5514510f4f
sneakyPent <zaharioudakis@yahoo.com>
2020-11-20 18:21:57 +0200

    utils.h code formatting

--- Assignment_4/src_corpus/utils.h
+++ Assignment_4/src_corpus/utils.h
@@ -1,7 +1,6 @@
 #ifndef _UTILS_H
 #define _UTILS_H
 
-
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -16,11 +15,12 @@
  * @brief Struct in linked list form for saving users in the
  *          Access Control Log Monitoring tool 
  */
-typedef struct userList {
-    int user;                                   /** The user */
-    struct filesList * filesNotAccessed;        /** List of files that user tried to access with no permission */
-    int mods;                                   /** The modifications the user has made on a given file */
-    struct userList * nextUser;                 /** Pointer to the next users of the UserList */
+typedef struct userList
+{
+    int user;                           /** The user */
+    struct filesList *filesNotAccessed; /** List of files that user tried to access with no permission */
+    int mods;                           /** The modifications the user has made on a given file */
+    struct userList *nextUser;          /** Pointer to the next users of the UserList */
 } userList;
 
 /**
@@ -30,24 +30,33 @@ typedef struct userList {
  */
 typedef struct filesList
 {
-    char fileName[3*BUF_LEN];       /** The current file name*/
-    int restFiles;                  /** The sum of files from list head until the end of the list */
-    struct filesList * nextFile;    /** Pointer to the next file of File list */
+    char fileName[3 * BUF_LEN]; /** The current file name*/
+    int restFiles;              /** The sum of files from list head until the end of the list */
+    struct filesList *nextFile; /** Pointer to the next file of File list */
 
 } filesList;
 
+enum mode
+{
+    info,
+    error,
+    success
+};
 
-enum mode{info,error,success};
-
-enum information{all, modifies, nonPermissions};
+enum information
+{
+    all,
+    modifies,
+    nonPermissions
+};
 
 filesList *addFile(filesList *head, char *fileName);
 
-userList * addUser(userList * head, int user, int modification, int notPermission, char *fileName);
+userList *addUser(userList *head, int user, int modification, int notPermission, char *fileName);
 
 void printUsers(userList *head, enum information printingInfo);
 
-char * getLineInfo(char * str);
+char *getLineInfo(char *str);
 
 char *trimwhitespace(char *str);
 
