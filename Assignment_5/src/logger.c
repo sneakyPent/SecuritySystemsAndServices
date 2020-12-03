2faa1f200fbd430f563c6d8fc9df1cac829f7cb2
sneakyPent <zaharioudakis@yahoo.com>
2020-12-03 18:50:53 +0200

    minor changes in logger.c

--- Assignment_5/src/logger.c
+++ Assignment_5/src/logger.c
@@ -50,6 +50,36 @@ fwrite(const void *ptr, size_t size, size_t nmemb, FILE *stream)
 	return original_fwrite_ret;
 }
 
+FILE *
+fopen64(const char *path, const char *mode)
+{
+
+	// return fopen(path, mode);
+	FILE *original_fopen_ret;
+	FILE *(*original_fopen)(const char *, const char *);
+	int exists = 1;
+	// Get the absolute path of the given file
+	char absolutePath[BUF_LEN];
+   	realpath(path, absolutePath);
+	
+	// If file does not exist update log file for file creation
+	if (access(absolutePath, F_OK) == -1)
+		exists = 0;
+	/* call the original fopen function */
+	original_fopen = dlsym(RTLD_NEXT, "fopen64");
+	original_fopen_ret = (*original_fopen)(absolutePath, mode);
+
+	if (original_fopen_ret)
+		if (exists == 0)	// Update log file for creating file
+			logFileUpdate(initLogs(absolutePath, creation, original_fopen_ret, mode));
+		else				// Update log file for opening file
+			logFileUpdate(initLogs(absolutePath, opening, original_fopen_ret, mode));
+	else
+		logFileUpdate(initLogs(absolutePath, opening, original_fopen_ret, mode));
+
+	return original_fopen_ret;
+}
+
 logEntry initLogs(const char *path, enum AccessType aType, FILE *file, const char *mode)
 {
 
@@ -139,32 +169,4 @@ int logFileUpdate(logEntry log)
 }
 
 
-FILE *
-fopen64(const char *path, const char *mode)
-{
 
-	// return fopen(path, mode);
-	FILE *original_fopen_ret;
-	FILE *(*original_fopen)(const char *, const char *);
-	int exists = 1;
-	// Get the absolute path of the given file
-	char absolutePath[BUF_LEN];
-   	realpath(path, absolutePath);
-	
-	// If file does not exist update log file for file creation
-	if (access(absolutePath, F_OK) == -1)
-		exists = 0;
-	/* call the original fopen function */
-	original_fopen = dlsym(RTLD_NEXT, "fopen64");
-	original_fopen_ret = (*original_fopen)(absolutePath, mode);
-
-	if (original_fopen_ret)
-		if (exists == 0)	// Update log file for creating file
-			logFileUpdate(initLogs(absolutePath, creation, original_fopen_ret, mode));
-		else				// Update log file for opening file
-			logFileUpdate(initLogs(absolutePath, opening, original_fopen_ret, mode));
-	else
-		logFileUpdate(initLogs(absolutePath, opening, original_fopen_ret, mode));
-
-	return original_fopen_ret;
-}
