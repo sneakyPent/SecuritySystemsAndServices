ba98eef01216ca57bbfeca7f067e20974c089dc8
sneakyPent <zaharioudakis@yahoo.com>
2020-12-14 02:20:20 +0200

    code format on monitor.c

--- Assignment_6/src/monitor.c
+++ Assignment_6/src/monitor.c
@@ -51,7 +51,6 @@ int main(int argc, char *argv[])
 }
 
 
-
 void decode_ip_header(const u_char *packet, int size)
 {
     unsigned short iphdrlen;
@@ -62,7 +61,7 @@ void decode_ip_header(const u_char *packet, int size)
     source.sin_addr.s_addr = ip_header->saddr;
     memset(&dest, 0, sizeof(dest));
     dest.sin_addr.s_addr = ip_header->daddr;
-    
+
     printf("| IP Header\n");
     printf("-> |-IP Version         : %d\n", (unsigned int)ip_header->version);
     printf("   |-IP Header Length   : %d Bytes\n", ((unsigned int)(ip_header->ihl)) * 4);
@@ -94,19 +93,17 @@ void decode_TCP(const u_char *packet, int size)
 void decode_UDP(const u_char *packet, int size)
 {
     unsigned short iphdrlen;
-	struct iphdr *iph = (struct iphdr *)(packet +  sizeof(struct ethhdr));
-	iphdrlen = iph->ihl*4;
-	struct udphdr *udph = (struct udphdr*)(packet + iphdrlen  + sizeof(struct ethhdr));
-	int header_size =  sizeof(struct ethhdr) + iphdrlen + sizeof udph;
-			
-	
-	printf("\n|UDP Header\n");
-	printf("-> |-Source Port      : %d\n" , ntohs(udph->source));
-	printf("   |-Destination Port : %d\n" , ntohs(udph->dest));
-	printf("   |-Header Length    : %d\n" ,  ntohs(udph->len));
-    printf("   |-Payload Length   : %d BYTES\n", size - header_size);
+    struct iphdr *iph = (struct iphdr *)(packet + sizeof(struct ethhdr));
+    iphdrlen = iph->ihl * 4;
+    struct udphdr *udph = (struct udphdr *)(packet + iphdrlen + sizeof(struct ethhdr));
+    int header_size = sizeof(struct ethhdr) + iphdrlen + sizeof udph;
 
-	printf("\n###########################################################\n\n");
+    printf("\n|UDP Header\n");
+    printf("-> |-Source Port      : %d\n", ntohs(udph->source));
+    printf("   |-Destination Port : %d\n", ntohs(udph->dest));
+    printf("   |-Header Length    : %d\n", ntohs(udph->len));
+    printf("   |-Payload Length   : %d BYTES\n", size - header_size);
+    printf("\n###########################################################\n\n");
 }
 
 void my_packet_handler(u_char *args, const struct pcap_pkthdr *header, const u_char *packet)
@@ -134,7 +131,8 @@ void my_packet_handler(u_char *args, const struct pcap_pkthdr *header, const u_c
     }
 }
 
-void printStatistics(){
+void printStatistics()
+{
     printf("\n\n");
     printf("Total number of packets received:  %d\n", restPackets + udpPackets + tcpPackets);
     printf("Total number of TCP packets received: %d\n", tcpPackets);
@@ -145,7 +143,7 @@ void printStatistics(){
 
 void handle_sigint(int sig)
 {
-    printStatistics();    
+    printStatistics();
     exit(1);
 }
 
@@ -153,22 +151,23 @@ void handle_sigint(int sig)
 void live_capture(const char *device)
 {
 
-    char error_buffer[PCAP_ERRBUF_SIZE];    /** Error buffer */
-    pcap_t *handle;                         /** The device handle from where we want to capture */
-    int packet_count_limit = 0;             /** The number of packets we want to capture ( 0 for unlimited packets) */
-    int timeout_limit = 1 * 1000;           /** In milliseconds */
+    char error_buffer[PCAP_ERRBUF_SIZE]; /** Error buffer */
+    pcap_t *handle;                      /** The device handle from where we want to capture */
+    int packet_count_limit = 0;          /** The number of packets we want to capture ( 0 for unlimited packets) */
+    int timeout_limit = 1 * 1000;        /** In milliseconds */
 
     /* Open device for live capture */
     handle = pcap_open_live(device, BUFSIZ, packet_count_limit, timeout_limit, error_buffer);
 
     if (handle == NULL)
         print(error_buffer, error);
-        
+
     /* Our function to output some info */
     pcap_loop(handle, packet_count_limit, my_packet_handler, NULL);
 }
 
-void offline_capture(const char *fname){
+void offline_capture(const char *fname)
+{
     char error_buffer[PCAP_ERRBUF_SIZE]; /** Error buffer */
     pcap_t *handle;                      /** The device handle from where we want to capture */
     int packet_count_limit = 0;          /** The number of packets we want to capture ( 0 for unlimited packets) */
