4306786ce636c2ed1ae3900e428f85e06bfc00db
sneakyPent <zaharioudakis@yahoo.com>
2020-12-14 02:06:08 +0200

    create function live_capture for capturing live packages with a given network interface

--- Assignment_6/src/monitor.c
+++ Assignment_6/src/monitor.c
@@ -48,6 +48,25 @@ int main(int argc, char *argv[])
     return 0;
 }
 
+
+void live_capture(const char *device)
+{
+
+    char error_buffer[PCAP_ERRBUF_SIZE];    /** Error buffer */
+    pcap_t *handle;                         /** The device handle from where we want to capture */
+    int packet_count_limit = 0;             /** The number of packets we want to capture ( 0 for unlimited packets) */
+    int timeout_limit = 1 * 1000;           /** In milliseconds */
+
+    /* Open device for live capture */
+    handle = pcap_open_live(device, BUFSIZ, packet_count_limit, timeout_limit, error_buffer);
+
+    if (handle == NULL)
+        print(error_buffer, error);
+        
+    /* Our function to output some info */
+    pcap_loop(handle, packet_count_limit, my_packet_handler, NULL);
+}
+
 void offline_capture(const char *fname){
     char error_buffer[PCAP_ERRBUF_SIZE]; /** Error buffer */
     pcap_t *handle;                      /** The device handle from where we want to capture */
