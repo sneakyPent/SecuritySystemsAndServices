7847ba255925bfed3f1a302313398926180b7d43
sneakyPent <zaharioudakis@yahoo.com>
2020-12-14 02:02:01 +0200

    create usage function and options switch in main

--- Assignment_6/src/monitor.c
+++ Assignment_6/src/monitor.c
@@ -1,7 +1,49 @@
 #include "monitor.h"
 
+void usage(void)
+{
+    printf(
+        "\n"
+        "usage:\n"
+        "\t./monitor \n"
+        "\nOptions:\n\n"
+        "-i <intf>,\t\t Name of the network interface will be monitored.\n"
+        "-r <pcap_file>,\t\t The Packet capture will be used\n"
+        "-h, \t\t\t Help message\n\n");
+
+    exit(1);
+}
+
 int main(int argc, char *argv[])
 {
+    signal(SIGINT, handle_sigint);
+    const char *fname;  /** Packet capture file name (e.g., test.pcap) */
+    const char *device; /** Network interface name (e.g., eth0)  */
+
+    int ch;
+    if (argc < 2 || argc > 3)
+        usage();
+    while ((ch = getopt(argc, argv, "i:r:h")) != -1)
+    {
+        switch (ch)
+        {
+        case 'i':
+            live_capture(optarg);
+            break;
+        case 'r':
+            offline_capture(optarg);
+            printStatistics();
+            break;
+        case 'h':
+            usage();
+            break;
+        default:
+            usage();
+        }
+    }
+
+    argc -= optind;
+    argv += optind;
 
     return 0;
 }
