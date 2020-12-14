16add7a9b6026fd0352615aad8978fe1089cb8fa
sneakyPent <zaharioudakis@yahoo.com>
2020-12-14 02:00:40 +0200

    create monitor.c and monitor.h files

---
+++ Assignment_6/src/monitor.c
@@ -0,0 +1,7 @@
+#include "monitor.h"
+
+int main(int argc, char *argv[])
+{
+
+    return 0;
+}
---
+++ Assignment_6/src/monitor.h
@@ -0,0 +1,20 @@
+#ifndef _MONITOR_H
+#define _MONITOR_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pcap.h>
+#include <signal.h>
+#include <net/if.h>
+#include <net/ethernet.h>
+#include <netinet/in.h>
+#include <netinet/ether.h>
+#include <netinet/if_ether.h>
+#include <netinet/tcp.h>
+#include <netinet/udp.h>
+#include <netinet/ip.h>
+#include <arpa/inet.h>
+
+
+#endif /* _MONITOR_H */
